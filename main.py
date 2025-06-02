76cf1b1b68e7f6593b21b408d4c1f9b28c65109b
ahmedaziz-cpu <2024_ahmed.aziz@iticbcn.cat>
2025-06-02 17:31:07 +0200

    exercici3

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, HTTPException
+from services.user import (
+    create_user,
+    get_user_by_id
+)
+
+app = FastAPI()
+
+
+@app.get("/users/{user_id}", response_model=dict)
+async def read_user(user_id: int):
+    user = get_user_by_id(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="no hi ha user amb aquest id")
+    return user
+
---
+++ services/user.py
@@ -0,0 +1,29 @@
+import connexion.db
+from schemas.user_sch import  user_schema
+from connexion.db import connection_db
+
+
+def get_user_by_id(user_id: int) -> dict | None:
+    conn = connexion.db.connection_db()
+    cursor = conn.cursor()
+    try:
+        sql = "SELECT id, nom, cognom, email, curs, ano, calle FROM users WHERE id = %s"
+        cursor.execute(sql, (user_id,))
+        row = cursor.fetchone()
+        return user_schema(row) if row else None
+    finally:
+        cursor.close()
+        conn.close()
+
+def create_user(nom: str, email: str, cognom: str, descripcio: str, curs: str, ano: int, calle: str, cp: int, password: str) -> dict:
+    conn = connection_db()
+    cursor = conn.cursor()
+    try:
+        sql = "INSERT INTO users (nom, cognom, email, descripcio, curs, ano, calle, cp, password) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id, nom, email"
+        cursor.execute(sql, (nom, cognom, email, descripcio, curs, ano, calle, cp, password))
+        new_row = cursor.fetchone()
+        conn.commit()
+        return user_schema(new_row)
+    finally:
+        cursor.close()
+        conn.close()
