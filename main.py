dd2d0a7037f2ccea875ae23d44c0e80d5e0efbed
ahmedaziz-cpu <2024_ahmed.aziz@iticbcn.cat>
2025-06-02 18:21:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 from fastapi import FastAPI
 from services.user import (
-    create_user,
-    get_user_by_id
+    update_user
 )
-
 
 app = FastAPI()
 
 
-@app.get("/users/{user_id}", response_model=dict)
-async def read_user(user_id: int):
-    user = get_user_by_id(user_id)
+@app.put("/users/{user_id}", response_model=dict)
+async def update_user_endpoint(user_id: int, name: str, email: str):
 
-    return user
-
-@app.post("/users", response_model=dict)
-async def create_user_endpoint(
-    nom: str, email: str, cognom: str, descripcio: str, curs: str, ano: int, calle: str, cp: int, password: str
-):
-    new_user = create_user(nom, email, cognom, descripcio, curs, ano, calle, cp, password)
-    return new_user
-
+    updated = update_user(user_id, name, email)
+    return updated
 
